dcdd763616a7bac49c1de81634ce707fa471482b
Bobbie Macdonald <bnjmacdonald@gmail.com>
2018-07-14 22:56:38 +0300

    remove old tensorflow classifiers
    These classifiers are replaced in subsequent commit with `tftxtclassify`
    classifiers.

--- hansardparser/plenaryparser/classify/tf/__init__.py
+++
@@ -1,3 +0,0 @@
-from hansardparser.plenaryparser.classify.tf.Classifier import Classifier
-from hansardparser.plenaryparser.classify.tf.Cbow import CbowClassifier
-from hansardparser.plenaryparser.classify.tf.Rnn import RnnClassifier
