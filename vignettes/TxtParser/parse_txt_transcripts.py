b600450091c5c5b5d666437c35ad0508cbb93427
Bobbie Macdonald <bnjmacdonald@gmail.com>
2018-07-15 18:56:57 +0300

    update `parse_txt_transcripts` with refactored TxtParser arguments

--- vignettes/TxtParser/parse_txt_transcripts.py
+++ vignettes/TxtParser/parse_txt_transcripts.py
@@ -23,17 +23,30 @@ import warnings
 from hansardparser.plenaryparser.TxtParser import TxtParser
 from hansardparser import settings
 
+# KLUDGE: for builder pickle imports.
+from hansardparser.plenaryparser.TxtParser.LineLabeler.SupervisedLineLabeler import *  
 
-INPATH = '/Users/bnjmacdonald/Documents/current/projects/hansardparser/data/tests/hansards/txt/1985/8th Oct 1985 - 11th dec 1985.txt'
+INPATH = '/Users/bnjmacdonald/Documents/current/projects/hansardparser/data/tests/hansards/txt/1985/1985.txt'
 OUTPATH = os.path.join(settings.DATA_ROOT, 'temp', 'plenaryparser', 'txt')
 VERBOSITY = 1
 MERGE = True
 TO_FORMAT = 'df-long'
 FOLDERS = ['1985', '1987', '1990', '1992']
 
+PREDICT_BATCH_SIZE = 100
+
 FILES_META_PATH = os.path.join(settings.DATA_ROOT, 'manual', 'txt-files-meta.json')
 with open(FILES_META_PATH, 'r') as f:
     FILES_META = json.load(f)
+
+BUILDER_PATH = os.path.join(settings.DATA_ROOT, 'generated', 'plenaryparser', 'text2vec', 'builders', '2018-07-15T084305')
+CLASSIFIER_PATH = os.path.join(settings.EXPERIMENTS_ROOT, 'plenaryparser', 'line_classifier', 'classifier0')
+LINE_LABEL_CODES_PATH = os.path.join(settings.DATA_ROOT, 'generated', 'plenaryparser', 'text2vec', 'corpora', '2018-07-15T084305', 'label_codes.json')
+with open(LINE_LABEL_CODES_PATH, 'r') as f:
+    LINE_LABEL_CODES = json.load(f)
+    assert len(set(LINE_LABEL_CODES.values())) == len(LINE_LABEL_CODES), \
+        'values must be unique for dict to be reversed.'
+    LINE_LABEL_CODES = {v: k for k, v in LINE_LABEL_CODES.items()}
 
 
 def main():
@@ -48,11 +61,17 @@ def main():
     try:
         start_line = FILES_META[year_str][filename]['start_line']
         end_line = FILES_META[year_str][filename]['end_line']
-        text = '\n'.join(text[start_line:end_line+1])
+        text = '\n'.join(text[start_line-1:end_line])
     except:
         warnings.warn(f'Start/end lines not found for "{year_str}/{filename}".')
         text = '\n'.join(text)
-    parser = TxtParser(verbose=VERBOSITY)
+    parser = TxtParser(
+        line_labeler='supervised',
+        line_predict_kws={'builder_path': BUILDER_PATH, 'clf_path': CLASSIFIER_PATH, 'batch_size': PREDICT_BATCH_SIZE},
+        line_label_codes=LINE_LABEL_CODES,
+        speaker_parser='rule',
+        verbosity=VERBOSITY
+    )
     # self = parser
     parsed_transcripts = parser.parse_hansards(
         text=text,
