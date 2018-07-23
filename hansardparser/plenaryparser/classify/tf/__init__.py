a56c7e603de2ed99c25ecd9fc984575a2882127a
Bobbie Macdonald <bnjmacdonald@gmail.com>
2018-07-22 23:00:54 -0700

    remove `classify` module
    The classify module is deprecated, as it is being replaced by the use of
    `tensor2tensor`.

--- hansardparser/plenaryparser/classify/tf/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import hansard_line_type4
-from . import hansard_line_has_speaker
-from . import hansard_line_speaker_span
--- scripts/predict_tf_classifier.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python -m hansardparser.plenaryparser.classify.tf.predict -v 2 \
-   --outpath data/tests/generated/plenaryparser/classify/tf/preds \
-   --documents data/tests/raw/hansard_lines.txt \
-   --builder data/generated/plenaryparser/text2vec/builders/2018-07-14T170136 \
-   --clf experiments/plenaryparser/line_classifier/classifier0
